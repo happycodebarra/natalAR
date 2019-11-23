e15134df87dd04df44aea633ecb61a428e80d3a9
PC 4 <53911823+happycodebarra@users.noreply.github.com>
2019-11-23 17:35:03 -0300

    Primeiro código

---
+++ NatalAR Project/NatalAR/Assets/scripts/Arvore.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arvore : MonoBehaviour
+{
+
+    [Header("Corpo da arvore")]
+    public GameObject[] troncos;
+    public GameObject[] vegetacoes;
+    public GameObject[] estrelas;
+    public GameObject[] enfeites;
+    public GameObject[] presentes;
+
+    [Header("Codigo da arvore")]
+    public int codTronco;
+    public int codVegetacao;
+    public int codEstrela;
+    public int codEnfeite;
+    public int codPresente;
+
+    public void carregarArvore()
+    {
+        for (int i = 0; i < troncos.Length; i++)
+        {
+            troncos[i].SetActive(false);
+        }
+
+
+
+        vegetacoes[codTronco].SetActive(false);
+        troncos[codTronco].SetActive(true);
+        vegetacoes[codVegetacao].SetActive(false);
+        estrelas[codEstrela].SetActive(false);
+        presentes[codPresente].SetActive(false);
+
+    }
+}
+
+
